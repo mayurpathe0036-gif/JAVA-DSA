bce9e33baa80708a3a8314ec926e1aa86545fb69
mayurpathe0036-gif <mayurpathe0036@gmail.com>
2026-06-27 21:26:20 +0530

    2nd

--- JAVA1.java
+++ JAVA1.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class JAVA1 {
     public static void main(String[] args) {
-        Scanner Scanner = new Scanner(System.in);
+        Scanner scanner= new Scanner(System.in);
         System.out.print("Enter your name: ");
-        String name = Scanner.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Hello, " + name + " world!");
     }
 }
---
+++ conditions.java
@@ -0,0 +1,12 @@
+public class conditions {
+    public static void main(String[] args) {
+        int salary = 14000;
+        if (salary > 10000) {
+            salary = salary + 2000;
+        } else if (salary > 5000) {
+            salary = salary + 1000;
+        } else {
+            System.out.println(salary);
+        }
+    }
+}
