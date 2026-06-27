8ffa77199de0420b1321c883aca6032d7aa89988
mayurpathe0036-gif <mayurpathe0036@gmail.com>
2026-06-27 20:31:24 +0530

    first

---
+++ JAVA1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class JAVA1 {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = Scanner.nextLine();
+        System.out.println("Hello, " + name + " world!");
+    }
+}
