6743387ed6c24cda9120c281bc07782c25ab813a
mayurpathe0036-gif <mayurpathe0036@gmail.com>
2026-06-28 23:57:48 +0530

    3

--- conditions.java
+++ conditions.java
@@ -1,12 +1,14 @@
 public class conditions {
     public static void main(String[] args) {
-        int salary = 14000;
+        /*int salary = 14000;
         if (salary > 10000) {
             salary = salary + 2000;
         } else if (salary > 5000) {
             salary = salary + 1000;
         } else {
             System.out.println(salary);
-        }
+
+        }*/
+       
     }
 }
---
+++ loops.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+    /*     // for loop
+        Scanner in= new Scanner(System.in);
+        int n = in.nextInt();
+        for (int num =0; num<= n; num++)
+            {
+             System.out.println(num+" hello");
+        }*/
+        // while loop
+       /* *Scanner in= new Scanner(System.in);
+        int n = in.nextInt();
+        int num = 0;
+        while (num<=n){
+            System.out.println(num+" ");
+            num ++;
+        }
+        // do while loop
+        int n=0;
+        do{
+             System.out.println(n+" ");
+             n++;
+        }while(n<=10);
+*/
+
+    } 
+}
